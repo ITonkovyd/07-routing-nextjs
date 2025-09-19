566387e5a0683acc3fc4b6ec10418c0feacd0351
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-09-19 17:03:14 +0300

    Update: removing unexpected files

--- app/(public routes)/notes/filter/page.tsx
+++ app/(public routes)/notes/filter/page.tsx
@@ -1,8 +1,11 @@
-import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { notFound } from "next/navigation";
-import { fetchNoteById } from "@/lib/api";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
 import NoteDetailsClient from "@/app/(public routes)/notes/[id]/NotePreview.client";
 import Modal from "@/components/Modal/Modal";
+import { fetchNoteById } from "@/lib/api";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -12,15 +15,10 @@ const NotePreview = async ({ params }: Props) => {
   const { id } = await params;
   const queryClient = new QueryClient();
 
-  try {
-    await queryClient.prefetchQuery({
-      queryKey: ["note", id],
-      queryFn: () => fetchNoteById(id),
-    });
-  } catch (error) {
-    // Якщо нотатка не знайдена, показуємо 404 сторінку
-    notFound();
-  }
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
 
   return (
     <Modal>
